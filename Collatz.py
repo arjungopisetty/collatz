77894dff1dda66d261034e3a992e4aca212b95ff
Arjun Gopisetty <arjung@cs.utexas.edu>
2017-01-31 21:55:04 -0600

    Final optimizations to meta cache solution

--- Collatz.py
+++ Collatz.py
@@ -10,8 +10,6 @@
 # Glenn P. Downing
 # ---------------------------
 
-from queue import Queue
-
 # ------------
 # collatz_read
 # ------------
@@ -31,17 +29,21 @@ def collatz_read (r) :
 # ------------
 
 cache = [1, 2, 3, 6, 7, 9, 18, 19, 25, 27, 54, 55, 73, 97, 129, 171, 231, 235, 313, 327, 649, 654, 655, 667, 703, 871, 1161, 2223, 2322, 2323, 2463, 2919, 3711, 6171, 10971, 13255, 17647, 17673, 23529, 26623, 34239, 35497, 35655, 52527, 77031, 106239, 142587, 156159, 216367, 230631, 410011, 511935, 626331, 837799, 1117065, 1126015, 1501353, 1564063, 1723519, 2298025, 3064033, 3542887, 3732423]
-max = 0
 
 def collatz_eval (n) :
     """
     n the end of the range [1, n], inclusive
     return the value that produces the max cycle length of the range [1, n]
     """
-    # <your code>
-    for i in reversed(range(1, n + 1)):
-        if i in cache:
-            return i
+    counter = 1
+
+    if (n >= 3732423):
+        return 3732423
+    
+    while True:
+        if cache[counter] > n:
+            return cache[counter - 1]
+        counter += 1
 
 def cycle_length (n) :
     c = 1
